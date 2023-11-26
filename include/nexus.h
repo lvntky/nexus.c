abd45b7dc436f8a44862e00c37a1bd990ebe2a8f
lvntky <leventkayadev@gmail.com>
2023-11-26 22:42:18 +0300

    [feature] tcp server established

--- include/nexus.h
+++
@@ -1,9 +0,0 @@
-#ifndef _NEXUS_H_
-#define _NEXUS_H_
-
-#include "server.h"
-#include "config.h"
-
-void nexus_banner_print();
-
-#endif
--- include/server.h
+++ include/server.h
@@ -1,12 +1,15 @@
 #ifndef _SERVER_H_
 #define _SERVER_H_
 
+#define MAX_SERVER_BACKLOG 8
+#define MAX_SOCKET_BUFFER_LEN 2048
+
 #include <sys/socket.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include "config.h"
 
-int tcp_establish_server_connection();
+void server_start();
 
 #endif
